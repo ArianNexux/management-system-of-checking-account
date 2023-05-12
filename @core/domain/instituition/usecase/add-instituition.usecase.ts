99a360ecd9f92be8d8aa8f2c3c74d74d39accf80
ArianNexux <ariannexux0101@gmail.com>
2023-05-12 01:06:28 +0100

    feat: test and implement usecase to add instituition

---
+++ @core/domain/instituition/usecase/add-instituition.usecase.ts
@@ -0,0 +1,45 @@
+import Id from "../../../@shared/value-objects/id.vo";
+import SizeLogo from "../../../@shared/value-objects/size-logo.vo";
+import Instituition from "../entities/instituition.entity";
+import InstituitionGateway from "../gateway/instituition.gateway";
+import { AddInstituitionInputDTO, AddInstituitionOutputDTO } from "../../../@shared/dtos/add-instituition.dto";
+
+export default class AddInstituitionUseCase {
+
+    constructor(
+        private repository: InstituitionGateway
+    ) {
+
+    }
+
+    async execute(input: AddInstituitionInputDTO): Promise<AddInstituitionOutputDTO> {
+        if (!input) {
+            throw new Error("Input DTO was not provided")
+        }
+
+        const propsInstitution = {
+            id: new Id(),
+            name: input.name,
+            title1: input.title1,
+            title2: input.title2,
+            logo: input.logo,
+            sizeLogo: new SizeLogo(input.heightLogo, input.widthLogo)
+        }
+
+        const instituition = new Instituition(propsInstitution)
+
+        await this.repository.add(instituition)
+
+
+        return {
+            id: instituition.id.id,
+            name: instituition.name,
+            title1: instituition.title1,
+            title2: instituition.title2,
+            widthLogo: instituition.sizeLogo.width,
+            heightLogo: instituition.sizeLogo.height,
+            logo: instituition.logo
+        }
+
+    }
+}
