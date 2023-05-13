54db0ecfd388cff223dc845f6a3c3c573efcf4be
ArianNexux <ariannexux0101@gmail.com>
2023-05-13 21:16:14 +0100

    feat: dto update instituition

---
+++ @core/@shared/dtos/update-instituition.dto.ts
@@ -0,0 +1,20 @@
+
+type UpdateInstituitionOutputDTO = {
+    id: string,
+    name: string,
+    logo: string,
+    heightLogo: number,
+    widthLogo: number,
+    title1: string,
+    title2: string
+}
+
+type UpdateInstituitionInputDTO = {
+    id: string,
+    name: string,
+    logo: string,
+    heightLogo: number,
+    widthLogo: number,
+    title1: string,
+    title2: string
+}
