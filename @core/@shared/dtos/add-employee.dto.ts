5fca493c126071ea05a9e4025994279c5a0ca1ec
ArianNexux <ariannexux0101@gmail.com>
2023-05-16 03:13:42 +0100

    feat: implement dto to add employee

---
+++ @core/@shared/dtos/add-employee.dto.ts
@@ -0,0 +1,16 @@
+export type AddEmployeeInputDTO = {
+    name: string;
+    email: string;
+    photo: string;
+    role: string;
+    position: string;
+}
+
+export type AddEmployeeOutputDTO = {
+    id: string
+    name: string;
+    email: string;
+    photo: string;
+    role: string;
+    position: string;
+}
